431c6342a8cb440eb4a7ffa22a510d67a657e770
dm1tro69 <dimolg22@gmail.com>
2020-05-02 18:24:51 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+         <header className='header'>
+                <img
+                    src="https://lh3.googleusercontent.com/proxy/AGZ0BWAAp4lyZ6pHltkPn3SNbjhjLQ4Y2OBMMIGBAQU6ikborvqdmzZujNYfPzIhMV6qKDKF0IyNtziuodgKjYr5zPJ4p1bHHk09E__xx7GrF3hTUeZPXA"
+                    alt=""/>
+            </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+                <div>
+
+                <img src="https://media.istockphoto.com/photos/microscopic-view-of-coronavirus-a-pathogen-that-attacks-the-tract-picture-id1200706447?k=6&m=1200706447&s=612x612&w=0&h=_Yi0dWqHn9tpO-MqC3jLg-qP66_e_8J-B9mO7Iejjqk=" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New Post
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                     <div>
+                        post1
+                    </div>
+                </div>
+            </div>
+
+    )
+}
+export default Profile
